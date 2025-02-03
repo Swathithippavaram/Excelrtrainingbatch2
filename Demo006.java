8ab24d4ab37bff65fde44252b2a823da7967ea52
Swathithippavaram <thippavaramswathi11@gmail.com>
2025-02-03 20:49:25 +0530

    Demo006.java

--- Demo006.java
+++ Demo006.java
@@ -1,9 +1,9 @@
 public class Demo006 {
     public static void main(String[] args) {
         String username, password;
-        username = "sindhu";
-        password = "sindhu@123";
-        while((username == "sindhu") && (password == "sindhu@123")) {
+        username = "swathi";
+        password = "swathi@123";
+        while((username == "swathi") && (password == "swathi@123")) {
             System.out.println("HAPPY NEW YEAR");
             break;
         }
